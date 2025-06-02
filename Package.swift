c7547abd48c67100d728bb4e676706f5a5f92307
Kevin CS <kevincarm016@hotmail.com>
2025-06-01 19:17:33 -0600

    fix: Disable TLS certificate verification, development purpose only

--- Package.swift
+++ Package.swift
@@ -12,7 +12,8 @@ let package = Package(
         // 🔵 Non-blocking, event-driven networking for Swift. Used for custom executors
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.65.0"),
         .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
-        .package(url: "https://github.com/vapor/fluent-mysql-driver.git", from: "4.0.0")
+        .package(url: "https://github.com/vapor/fluent-mysql-driver.git", from: "4.0.0"),
+        .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.20.0")
     ],
     targets: [
         .executableTarget(
--- Sources/SwiftBackend/configure.swift
+++ Sources/SwiftBackend/configure.swift
@@ -4,11 +4,18 @@ import FluentMySQLDriver
 
 // configures your application
 public func configure(_ app: Application) async throws {
-    // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-
     // register routes
     try routes(app)
+    //These lines are only for development purpose only
+    var tlsConfig = TLSConfiguration.makeClientConfiguration()
+    tlsConfig.trustRoots = .none
+    tlsConfig.certificateVerification = .none
     //database connection
-    app.databases.use(.mysql(hostname: "localhost", username: "root", password: "12345678", database: "SWIFT_BACKEND"), as: .mysql)
+    app.databases.use(.mysql(
+        hostname: "localhost",
+        username: "root",
+        password: "12345678",
+        database: "SWIFT_BACKEND",
+        tlsConfiguration: tlsConfig
+    ), as: .mysql)
 }
