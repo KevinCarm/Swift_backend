e6fdf02226588d8c746aa0a82652e1249661a98b
Kevin CS <kevincarm016@hotmail.com>
2025-06-08 10:10:01 -0600

    feat: Get user by id route

--- Sources/SwiftBackend/routes.swift
+++ Sources/SwiftBackend/routes.swift
@@ -1,4 +1,5 @@
 import Vapor
+import Fluent
 
 func routes(_ app: Application) throws {
     app.post("user") { req async throws -> User in
@@ -6,5 +7,12 @@ func routes(_ app: Application) throws {
         let input: User = try req.content.decode(User.self)
         try await input.create(on: req.db)
         return input
+    }
+    app.get("user", ":id") { req async throws -> User in
+        let id: String = req.parameters.get("id")!
+        guard let foundUser = try await User.find(Int(id), on: req.db) else {
+            throw Abort(.notFound, reason: "User not found")
+        }
+        return foundUser
     }
 }
